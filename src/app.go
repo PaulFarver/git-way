60d1bf55130ec56dddfc863a53cda8e7ed7dde7a
Paul Farver <PaulFarver@Gmail.com>
2019-03-24 14:22:19 +0100

    [Backend] Multiple prioritized branches

--- src/app.go
+++ src/app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"time"
 
 	"gopkg.in/src-d/go-git.v4"
@@ -15,8 +16,7 @@ import (
 type GraphNode struct {
 	Hash      string   `json:"id"`
 	Title     string   `json:"title"`
-	Type      string   `json:"type"`
-	IsMain    bool     `json:"main"`
+	Branch    string   `json:"branch"`
 	Amount    int      `json:"amount"`
 	Parents   []string `json:"parentIds"`
 	Timestamp int64    `json:"timestamp"`
@@ -38,6 +38,12 @@ func ensureRepo() *git.Repository {
 		})
 	}
 	check(err)
+	err = repo.Fetch(&git.FetchOptions{
+		Progress: os.Stdout,
+	})
+	if err != git.NoErrAlreadyUpToDate {
+		check(err)
+	}
 	return repo
 }
 
@@ -56,128 +62,89 @@ func getCheckTime(currTime time.Time, duration string) time.Time {
 func main() {
 	fmt.Println("Checking out...")
 	repo := ensureRepo()
-	commits := getCommits(repo)
-	checktime := getCheckTime(time.Now(), "3300h")
+	checktime := getCheckTime(time.Now(), "744h")
 
-	// ignored := make(map[plumbing.Hash]bool)
-	// endNodes := make(map[plumbing.Hash]bool)
-	// children := make(map[plumbing.Hash]int)
-
-	// commits.ForEach(func(c *object.Commit) error {
-	// 	if c.Author.When.Before(checktime) {
-	// 		ignored[c.Hash] = true
-	// 	}
-	// 	c.Parents().ForEach(func(p *object.Commit) error {
-	// 		children[p.Hash]++
-	// 		if children[p.Hash] > 1 {
-	// 			c.Parents().ForEach(func(p2 *object.Commit) error {
-	// 				endNodes[p2.Hash] = true
-	// 				return nil
-	// 			})
-	// 		}
-	// 		return nil
-	// 	})
-	// 	return nil
-	// })
-
-	// startNodes := make(map[plumbing.Hash]bool)
-	// commits.ForEach(func(c *object.Commit) error {
-	// 	if c.Author.When.Before(checktime) {
-	// 		return nil
-	// 	}
-	// 	if c.NumParents() != 1 {
-	// 		startNodes[c.Hash] = true
-	// 	}
-	// 	for _, p := range c.ParentHashes {
-	// 		pc, _ := repo.CommitObject(p)
-	// 		if !pc.Author.When.Before(checktime) {
-	// 			children[p]++
-	// 			if children[p] > 1 || startNodes[c.Hash] {
-	// 				endNodes[p] = true
-	// 			}
-	// 		} else {
-	// 			startNodes[c.Hash] = true
-	// 		}
-	// 	}
-	// 	return nil
-	// })
-
-	// Mark references as important
-	master := plumbing.Hash{}
-
+	branchKeys := [][]string{[]string{}, []string{}, []string{}, []string{}, []string{}, []string{}}
+	branches := make(map[string]plumbing.Hash)
 	refIter, err := repo.References()
+	fmt.Println("Discovering remote branches...")
+
 	check(err)
 	refIter.ForEach(func(r *plumbing.Reference) error {
-		if r.Name() == plumbing.Master {
-			master = r.Hash()
+		if r.Name().IsRemote() {
+			branches[r.Name().Short()] = r.Hash()
+			branchKeys[AssignPriority(r.Name())] = append(branchKeys[AssignPriority(r.Name())], r.Name().Short())
 		}
 		return nil
 	})
 	// Collect commits
-	fmt.Println("Walking master branch")
-	
-
-	// for k := range endNodes {
-
-	// 	if k.IsZero() || ignored[k] {
-	// 		continue
-	// 	}
-	// 	fmt.Println("endNode " + k.String())
-	// 	c, _ := repo.CommitObject(k)
-	// 	nCommits := 0
-	// 	p := c
-	// 	t := "none"
-	// 	parents := []string{}
-	// 	for {
-	// 		// Add parent hashes, if they are not ignored
-	// 		// If only one parent exists, try again for that one
-	// 		parents = []string{}
-	// 		pp, _ := p.Parent(0)
-	// 		if p.NumParents() == 1 && !ignored[pp.Hash] && !endNodes[pp.Hash] {
-	// 			p = pp
-	// 			nCommits++
-	// 			continue
-	// 		}
-	// 		for _, gp := range p.ParentHashes {
-	// 			if endNodes[gp] && !ignored[gp] && !gp.IsZero() {
-	// 				parents = append(parents, gp.String())
-	// 				continue
-	// 			}
-	// 		}
-	// 		break
-	// 	}
-	// 	g := GraphNode{Parents: parents, Type: t, Hash: c.Hash.String(), Amount: nCommits, Timestamp: c.Author.When.Unix()}
-	// 	// graphs[g.Hash] = g
-	// 	graphs = append(graphs, g)
-	// }
-
-	addRoot := false
 	graphs := []GraphNode{}
-	fmt.Println("Collecting commits")
-	commits.ForEach(func(c *object.Commit) error {
-		if c.Author.When.Before(checktime) {
-			return nil
+	branchcommits := []GraphNode{}
+	excludes := make(map[plumbing.Hash]bool)
+
+	for _, pBranches := range branchKeys {
+		for _, name := range pBranches {
+			fmt.Println("Walking branch: " + name)
+			hash := branches[name]
+			c, _ := repo.CommitObject(hash)
+			branchcommits, excludes, _ = WalkCommit(c, excludes, checktime, GraphNode{Branch: name})
+			fmt.Printf("found %v commits\n", len(branchcommits))
+			graphs = append(graphs, branchcommits...)
 		}
-		parents := []string{}
-		c.Parents().ForEach(func(p *object.Commit) error {
-			if !p.Author.When.Before(checktime) {
-				parents = append(parents, p.Hash.String())
-			}
-			return nil
-		})
-		if len(parents) == 0 {
-			// parents = append(parents, "root")
-		}
-		graphs = append(graphs, GraphNode{Hash: c.Hash.String(), Parents: parents})
-		return nil
-	})
-	if addRoot {
-		graphs = append(graphs, GraphNode{Hash: "root"})
 	}
+
+	// graphs = append(graphs, allcommits...)
 
 	// Write to file
 	b, err := json.Marshal(graphs)
 	check(err)
 	err = ioutil.WriteFile("www/graph.json", b, 0644)
 	check(err)
+}
+
+func WalkCommit(commit *object.Commit, excludes map[plumbing.Hash]bool, checktime time.Time, template GraphNode) ([]GraphNode, map[plumbing.Hash]bool, bool) {
+	// Check if commit has been parsed before
+	if commit.Committer.When.Before(checktime) {
+		return nil, excludes, false
+	}
+	if excludes[commit.Hash] {
+		return nil, excludes, true
+	}
+	graphs := []GraphNode{}
+	parentIds := []string{}
+	// Parse parents
+	commit.Parents().ForEach(func(parent *object.Commit) error {
+		parentGraphs, ex, include := WalkCommit(parent, excludes, checktime, template)
+		excludes = ex
+		graphs = append(graphs, parentGraphs...)
+		if include {
+			parentIds = append(parentIds, parent.Hash.String())
+		}
+		return nil
+	})
+	g := template
+	g.Hash = commit.Hash.String()
+	g.Parents = parentIds
+	g.Timestamp = commit.Committer.When.Unix()
+	excludes[commit.Hash] = true
+	return append(graphs, g), excludes, true
+}
+
+var hotfixregex = regexp.MustCompile(`hotfix\/(.+)`)
+var featureregex = regexp.MustCompile(`feature\/(.+)`)
+
+func AssignPriority(branch plumbing.ReferenceName) int {
+	if featureregex.Match([]byte(branch)) {
+		return 3
+	}
+	if hotfixregex.Match([]byte(branch)) {
+		return 1
+	}
+	if branch.Short() == "master" || branch.Short() == "origin/master" {
+		return 0
+	}
+	if branch.Short() == "develop" || branch.Short() == "origin/develop" {
+		return 2
+	}
+	return 4
 }
