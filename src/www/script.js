96cef3dd734a4dea7d5c4f698b74223074e46c37
Paul Farver <PaulFarver@Gmail.com>
2019-04-14 01:04:07 +0200

    [script.js] Extract swimlane rendering

--- src/www/script.js
+++ src/www/script.js
@@ -55,7 +55,6 @@ pullfunction = function() {
     response
   ) {
     let a = performance.now();
-    // let svg = d3.select("#graph");
     render(JSON.parse(response));
     let b = performance.now();
     console.log(`Graph rendered in ${Math.round((b - a) * 10) / 10} ms`);
@@ -76,149 +75,166 @@ pullfunction = function() {
   function getX(timestamp, min, max, width) {
     return (width * (timestamp - min)) / (max - min);
   }
-
-  function drawlanes(svg, branches, width, min) {
-    ydiff = diff - 6;
-    swimlanes = svg.selectAll(".branchlane").data(branches, b => b.name);
-    swimlanes.exit().remove()
-    g = swimlanes
-      .enter()
-      .append("g")
-      .attr("class", "branchlane");
-    g.append("rect")
-      .attr("y", branch => getY(branch.name) - diff / 2)
-      .attr("width", width)
-      .attr("height", diff)
-      .attr("class", "swimlane")
-      .attr("priority", branch => branch.priority);
-    g.append("foreignObject")
-      .attr("x", width - 200)
-      .attr("y", branch => getY(branch.name) - ydiff / 2)
-      .attr("width", 200)
-      .attr("height", ydiff / 3)
-      .append("xhtml:body")
-      .attr("class", "branchlabel branchname")
-      .style("line-height", ydiff / 3 + "px")
-      .html(branch => removePrefix(branch.name));
-    g.append("foreignObject")
-      .attr("x", width - 200)
-      .attr("y", branch => getY(branch.name) - ydiff / 2 + ydiff / 3)
-      .attr("width", 200)
-      .attr("height", ydiff / 3)
-      .append("xhtml:body")
-      .attr("class", "branchlabel branchauthor")
-      .style("line-height", ydiff / 3 + "px")
-      .html(branch => branch.lastcommitter);
-    g.append("foreignObject")
-      .attr("x", width - 200)
-      .attr("y", branch => getY(branch.name) - ydiff / 2 + (ydiff * 2) / 3)
-      .attr("width", 200)
-      .attr("height", ydiff / 3)
-      .append("xhtml:body")
-      .attr("class", "branchlabel branchtime")
-      .style("line-height", ydiff / 3 + "px")
-      .html(branch => elapsed(branch.lastcommit));
-
-    swimlanes.transition().selectAll("foreignObject body.branchlabel.branchname").text(d => removePrefix(d.name))
-    swimlanes.transition().selectAll("foreignObject body.branchlabel.branchauthor").text(d => d.lastcommitter)
-    swimlanes.transition().selectAll("foreignObject body.branchlabel.branchtime").text(d => elapsed(d.lastcommit))
-  }
-
-  function drawlines(svg, links, nodes, min, max, width) {
-    l = svg
-      .selectAll(".commitline")
-      .data(links, (link) => link.source+link.target);
-
-    l.exit().remove();
-
-    function updatePosition(selection) {
-      selection
-        .attr("x1", link => getX(nodes[link.source].timestamp, min, max, width))
-        .attr("x2", link => getX(nodes[link.target].timestamp, min, max, width))
-        .attr("y1", link => getY(nodes[link.source].branch))
-        .attr("y2", link => getY(nodes[link.target].branch))
-        .attr("prehistoric", link => nodes[link.target].timestamp == 0);
-    }
-
-    updatePosition(
-      l
-        .enter()
-        .append("line")
-        .attr("class", "commitline")
-    );
-
-    updatePosition(l.transition());
-  }
-
-  function drawnodes(svg, nodes, min, max, width, relevants) {
-    n = svg
-      .selectAll(".commitobject")
-      .data(Object.keys(nodes), d => d);
-    n.exit().remove();
-
-    function updatePosition(selection) {
-      selection
-        .attr("r", node => (relevants[node] ? 6 : 0))
-        .attr("important", node => (relevants[node] ? true : false))
-        .attr("transform", node => {
-          let x = getX(nodes[node].timestamp, min, max, width);
-          let y = getY(nodes[node].branch);
-          return `translate(${x}, ${y})`;
-        });
-    }
-
-    updatePosition(
-      n
-        .enter()
-        .append("g")
-        .attr("class", "commitobject")
-        .append("svg:circle")
-        .attr("class", "commitnode")
-    );
-
-    updatePosition(n.transition().selectAll(".commitnode"));
-  }
-
-  function render(graph) {
-    let svg = d3.select("#graph");
-
-    let width = 1800;
-    let padding = 0;
-
-    graph.branches.forEach(branch => {
-      getY(branch.name);
-    });
-
-    drawlanes(svg, graph.branches, width, graph.mintime);
-
-    // var height = getY("final") - diff / 2;
-    var height = 700;
-    svg.attr(
-      "viewBox",
-      `-${padding} -${padding} ${width + padding * 2} ${height + padding * 2}`
-    );
-
-    drawlines(
-      svg,
-      graph.links,
-      graph.nodes,
-      graph.mintime,
-      graph.maxtime,
-      width - 200
-    );
-
-    drawnodes(
-      svg,
-      graph.nodes,
-      graph.mintime,
-      graph.maxtime,
-      width - 200,
-      graph.relevants
-    );
-  }
 };
 
+function render(graph) {
+  let svg = d3.select("#graph");
+
+  let width = 1800;
+  let padding = 0;
+
+  let branchYs = {};
+  let diff = 60;
+  let curry = diff / 2;
+
+  let y = branch => {
+    if (branchYs[branch] == null) {
+      branchYs[branch] = curry;
+      curry += diff;
+    }
+    return branchYs[branch];
+  };
+
+  xfactor = (width - 200) / (graph.maxtime - graph.mintime);
+  let x = timestamp => {
+    return (timestamp - graph.mintime) * xfactor;
+  };
+
+  graph.branches.forEach(branch => {
+    y(branch.name);
+  });
+
+  drawlanes(svg, graph.branches, width, diff, y);
+
+  let height = y("final");
+  svg.attr(
+    "viewBox",
+    `-${padding} -${padding} ${width + padding * 2} ${height + padding * 2}`
+  );
+
+  drawlines(
+    svg,
+    graph.links,
+    graph.nodes,
+    graph.mintime,
+    graph.maxtime,
+    width - 200
+  );
+
+  drawnodes(
+    svg,
+    graph.nodes,
+    graph.mintime,
+    graph.maxtime,
+    width - 200,
+    graph.relevants
+  );
+}
+
+function drawlanes(svg, branches, width, diff, y) {
+  let ydiff = diff - 6;
+  let swimlanes = svg.selectAll(".branchlane").data(branches, b => b.name);
+  swimlanes.exit().remove();
+  g = swimlanes
+    .enter()
+    .append("g")
+    .attr("class", "branchlane")
+    .attr("transform", b => `translate(0, ${y(b.name) - diff / 2})`);
+  g.append("rect")
+    .attr("width", width)
+    .attr("height", diff)
+    .attr("class", "swimlane")
+    .attr("priority", branch => branch.priority);
+  g.append("foreignObject")
+    .attr("x", width - 200)
+    .attr("y",  0)
+    .attr("width", 200)
+    .attr("height", ydiff / 3)
+    .append("xhtml:body")
+    .attr("class", "branchlabel branchname")
+    .style("line-height", ydiff / 3 + "px")
+    .text(b => removePrefix(b.name));
+  g.append("foreignObject")
+    .attr("x", width - 200)
+    .attr("y",  ydiff / 3)
+    .attr("width", 200)
+    .attr("height", ydiff / 3)
+    .append("xhtml:body")
+    .attr("class", "branchlabel branchauthor")
+    .style("line-height", ydiff / 3 + "px")
+    .text(b => b.lastcommitter);
+  g.append("foreignObject")
+    .attr("x", width - 200)
+    .attr("y", ydiff * 2 / 3)
+    .attr("width", 200)
+    .attr("height", ydiff / 3)
+    .append("xhtml:body")
+    .attr("class", "branchlabel branchtime")
+    .style("line-height", ydiff / 3 + "px")
+    .text(b => elapsed(b.lastcommit));
+    
+  swimlanes
+    .transition()
+    .selectAll("foreignObject body.branchlabel.branchauthor")
+    .text(d => d.lastcommitter);
+  swimlanes
+    .transition()
+    .selectAll("foreignObject body.branchlabel.branchtime")
+    .text(d => elapsed(d.lastcommit));
+}
+
+function drawlines(svg, links, nodes, min, max, width) {
+  let l = svg
+    .selectAll(".commitline")
+    .data(links, link => link.source + link.target);
+
+  l.exit().remove();
+
+  function updatePosition(selection) {
+    selection
+      .attr("x1", link => getX(nodes[link.source].timestamp, min, max, width))
+      .attr("x2", link => getX(nodes[link.target].timestamp, min, max, width))
+      .attr("y1", link => getY(nodes[link.source].branch))
+      .attr("y2", link => getY(nodes[link.target].branch))
+      .attr("prehistoric", link => nodes[link.target].timestamp == 0);
+  }
+
+  updatePosition(
+    l
+      .enter()
+      .append("line")
+      .attr("class", "commitline")
+  );
+
+  updatePosition(l.transition());
+}
+
+function drawnodes(svg, nodes, min, max, width, relevants) {
+  n = svg.selectAll(".commitobject").data(Object.keys(nodes), d => d);
+  n.exit().remove();
+
+  function updatePosition(selection) {
+    selection
+      .attr("r", node => (relevants[node] ? 6 : 0))
+      .attr("important", node => (relevants[node] ? true : false))
+      .attr("transform", node => {
+        let x = getX(nodes[node].timestamp, min, max, width);
+        let y = getY(nodes[node].branch);
+        return `translate(${x}, ${y})`;
+      });
+  }
+
+  updatePosition(
+    n
+      .enter()
+      .append("g")
+      .attr("class", "commitobject")
+      .append("svg:circle")
+      .attr("class", "commitnode")
+  );
+
+  updatePosition(n.transition().selectAll(".commitnode"));
+}
+
 pullfunction();
-setInterval(function() {
-  pullfunction();
-}, 10000);
+setInterval(pullfunction, 10000);
