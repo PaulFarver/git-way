3dfb44e035357bcb5e59c239d27ae49f095d4a4f
Paul Farver <PaulFarver@Gmail.com>
2019-04-14 13:40:40 +0200

    [script.js] Refactoring

--- src/www/script.js
+++ src/www/script.js
@@ -109,51 +109,48 @@ function drawlanes(svg, branches, width, diff, y) {
   let ydiff = diff - 6;
   let swimlanes = svg.selectAll(".branchlane").data(branches, b => b.name);
   swimlanes.exit().remove();
+
+  let t = b => `translate(0, ${y(b.name) - diff / 2})`;
+
   g = swimlanes
     .enter()
     .append("g")
     .attr("class", "branchlane")
-    .attr("transform", b => `translate(0, ${y(b.name) - diff / 2})`);
+    .attr("transform", t);
   g.append("rect")
     .attr("width", width)
     .attr("height", diff)
     .attr("class", "swimlane")
     .attr("priority", branch => branch.priority);
-  g.append("foreignObject")
+  labels = g
+    .append("foreignObject")
     .attr("x", width - 200)
-    .attr("y", 0)
+    .attr("y", 3)
+    .attr("height", ydiff)
     .attr("width", 200)
-    .attr("height", ydiff / 3)
-    .append("xhtml:body")
+    .append("xhtml:body");
+  labels
+    .append("p")
     .attr("class", "branchlabel branchname")
-    .style("line-height", ydiff / 3 + "px")
     .text(b => removePrefix(b.name));
-  g.append("foreignObject")
-    .attr("x", width - 200)
-    .attr("y", ydiff / 3)
-    .attr("width", 200)
-    .attr("height", ydiff / 3)
-    .append("xhtml:body")
+  labels
+    .append("p")
     .attr("class", "branchlabel branchauthor")
-    .style("line-height", ydiff / 3 + "px")
     .text(b => b.lastcommitter);
-  g.append("foreignObject")
-    .attr("x", width - 200)
-    .attr("y", (ydiff * 2) / 3)
-    .attr("width", 200)
-    .attr("height", ydiff / 3)
-    .append("xhtml:body")
+  labels
+    .append("p")
     .attr("class", "branchlabel branchtime")
-    .style("line-height", ydiff / 3 + "px")
     .text(b => elapsed(b.lastcommit));
+
+  swimlanes.transition().attr("transform", t);
 
   swimlanes
     .transition()
-    .selectAll("foreignObject body.branchlabel.branchauthor")
+    .selectAll("foreignObject body p.branchlabel.branchauthor")
     .text(d => d.lastcommitter);
   swimlanes
     .transition()
-    .selectAll("foreignObject body.branchlabel.branchtime")
+    .selectAll("foreignObject body p.branchlabel.branchtime")
     .text(d => elapsed(d.lastcommit));
 }
 
@@ -164,54 +161,49 @@ function drawlines(svg, links, nodes, x, y) {
 
   lines.exit().remove();
 
-  function updatePosition(selection) {
-    selection
-      .attr("x1", link => x(nodes[link.source].timestamp))
-      .attr("x2", link => x(nodes[link.target].timestamp))
-      .attr("y1", link => y(nodes[link.source].branch))
-      .attr("y2", link => y(nodes[link.target].branch))
-      .attr("prehistoric", link => nodes[link.target].timestamp == 0);
-  }
+  lines
+    .enter()
+    .append("line")
+    .attr("class", "commitline")
+    .attr("x1", l => x(nodes[l.source].timestamp))
+    .attr("x2", l => x(nodes[l.target].timestamp))
+    .attr("y1", l => y(nodes[l.source].branch))
+    .attr("y2", l => y(nodes[l.target].branch))
+    .attr("prehistoric", l => nodes[l.target].timestamp == 0);
 
-  updatePosition(
-    lines
-      .enter()
-      .append("line")
-      .attr("class", "commitline")
-  );
-
-  updatePosition(lines.transition());
+  lines
+    .transition()
+    .attr("x1", l => x(nodes[l.source].timestamp))
+    .attr("x2", l => x(nodes[l.target].timestamp))
+    .attr("y1", l => y(nodes[l.source].branch))
+    .attr("y2", l => y(nodes[l.target].branch))
+    .attr("prehistoric", l => nodes[l.target].timestamp == 0);
 }
 
 function drawnodes(svg, nodes, relevants, x, y) {
   let selects = svg.selectAll(".commitobject").data(Object.keys(nodes), d => d);
   selects.exit().remove();
 
-  function updatePosition(selection) {
-    return selection.attr(
-      "transform",
-      n => `translate(${x(nodes[n].timestamp)}, ${y(nodes[n].branch)})`
-    );
-  }
+  let t = n => `translate(${x(nodes[n].timestamp)}, ${y(nodes[n].branch)})`;
+  let r = n => (relevants[n] ? 6 : 0);
+  let i = n => (relevants[n] ? true : false);
 
-  function updateCircles(selection) {
-    return selection
-      .attr("r", node => (relevants[node] ? 6 : 0))
-      .attr("important", node => (relevants[node] ? true : false));
-  }
-
-  objects = selects
+  selects
     .enter()
     .append("g")
-    .attr("class", "commitobject");
-  circles = updatePosition(objects)
+    .attr("class", "commitobject")
+    .attr("transform", t)
     .append("svg:circle")
-    .attr("class", "commitnode");
-  updateCircles(circles);
+    .attr("class", "commitnode")
+    .attr("r", r)
+    .attr("important", i);
 
-  objects = selects.transition();
-  updatePosition(objects);
-  updateCircles(objects.select(".commitnode"));
+  selects
+    .transition()
+    .attr("transform", t)
+    .select(".commitnode")
+    .attr("r", r)
+    .attr("important", i);
 }
 
 pullfunction();
